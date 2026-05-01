46b1efa0573e45c5d270679b3a5448f0442a10be
Alberto.G <145074162+Gspidie@users.noreply.github.com>
2026-05-01 02:01:23 -0500

    Create Account class with initialization

---
+++ account.py
@@ -0,0 +1,8 @@
+class Account:
+    def __init__(self, id = None, first_name = None, last_name = None, account_number = None):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.account_number = account_number
+
+local_account = Account()
